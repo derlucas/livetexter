dd31074ce207e3e9179e940b981b3eb20365a739
Lucas Pleß <lucas@xleave.de>
2016-03-29 13:59:58 +0200

    initial commit

---
+++ src/main/java/de/derlucas/livetexter/Application.java
@@ -0,0 +1,18 @@
+package de.derlucas.livetexter;
+
+public class Application {
+
+    private ControlForm controlForm;
+
+    public static void main(String[] args) {
+        new Application();
+    }
+
+    public Application() {
+
+
+        controlForm = new ControlForm();
+
+    }
+
+}
